9eedbe6b84d65f93b71a6201d3152796fc68858d
Rahul <rahulchodvadiya@gmail.com>
2023-06-27 20:10:01 +0530

    add architecture diagram & boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DashboardCraft
 Create interactive and customizable dashboards with real-time data.
+
+## Architecture diagram
+
+![alt text](/dynamic-dashboard.svg)
+
+## Development
+
+Install dependencies
+> yarn
+
+Start app
+> yarn run dev
---
+++ dashboard-craft-api/src/models/widget.model.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Author, Genre } from './';
+import { type } from 'os';
+
+export type BookDocument = Book & Document;
+
+@Schema()
+export class Book {
+  @Prop({ required: true, unique: true })
+  title: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true })
+  author: Author;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Genre', required: true })
+  genre: Genre;
+
+  @Prop()
+  publishDate: Date;
+
+  type
+
+  configuration
+
+  associated_dashboard
+
+  
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
